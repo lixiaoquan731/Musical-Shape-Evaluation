55ac25d0ea9f2b1342029af6e75d33f4950e7496
lixiaoquan731 <55831928+lixiaoquan731@users.noreply.github.com>
2023-06-09 20:12:15 +0100

    Delete config_SRNN.py

--- config_SRNN.py
+++
@@ -1,7 +0,0 @@
-training_dir = "D:/Project/SRNN/Data/Train"
-training_csv = "D:/Project/SRNN/Data//train_data.xlsx"
-testing_csv = "D:/Project/SRNN/Data//test_data.xlsx"
-testing_dir = "D:/Project/SRNN/Data/Test"
-
-batch_size = 32
-epochs = 200
